6d6877557969014cb5f8c1d32e1002d051dd0d75
J-Fannilla <jose@fannilla.com>
2024-02-18 15:45:29 +0100

    test cache

--- packages/fan-tests/web/tests/example.spec.ts
+++ packages/fan-tests/web/tests/example.spec.ts
@@ -8,6 +8,7 @@ test.describe('Testing the web app', async () => {
   test('The web app should load correctly', async ({ page }) => {
     const button = page.getByRole('button', { name: 'Click web!' });
 
+    // Click the button and take a screenshot
     await expect(button).toBeVisible();
   });
 });
